4e581bba554277df09745d1a4d4fdef00e9babec
Moscoder99 <osullivan.martina@gmail.com>
2020-08-14 12:17:04 +0000

    added allauth functionality

--- project4/settings.py
+++ project4/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #allauth requirement
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -59,13 +64,38 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', #allauth requirement
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+#allauth requirement
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+#allauth requirement
+SITE_ID = 1
+EMAIL_BACKEND =  'django.core.mail.backends.console.EmailBackend'
+
+#allow authentication using usernames or email addresses
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+#add email as mandatory and email verification
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+#set a minimum length of 4 for the username
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+#spefify logon and redirect urls
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'project4.wsgi.application'
 
