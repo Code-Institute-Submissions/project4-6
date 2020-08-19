f1f807891d49563b8ea4d466cd47b46ddd4171fe
Moscoder99 <osullivan.martina@gmail.com>
2020-08-19 10:18:27 +0000

    updates to navbar

--- project4/settings.py
+++ project4/settings.py
@@ -153,3 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# Note: Static root not created as it will interfere with AWS integration
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
