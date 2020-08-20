1f2baeabf424af2fad0a39fa15069e0fe9b70621
Moscoder99 <osullivan.martina@gmail.com>
2020-08-20 15:12:47 +0000

    added restaurant data and view

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
 from .models import Restaurant, Cuisine, Location
 
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'cuisine',
+        'location',
+        'discount',
+        'image',
+    )
+
+    ordering = ('name',)
+
 # Register your models here.
 
-admin.site.register(Cuisine)
-admin.site.register(Location)
-admin.site.register(Restaurant)
-
+admin.site.register(Cuisine, CuisineAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Restaurant
 
 # Create your views here.
+
+def restaurants(request):
+
+    restaurants = Restaurant.objects.all()
+
+    context = {
+        'restaurants': restaurants,
+    }
+
+    return render(request, 'restaurants/restaurants.html', context)
