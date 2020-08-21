44888f0865ee661b48f7d5d23096c0e99e6e57dd
Moscoder99 <osullivan.martina@gmail.com>
2020-08-21 22:54:25 +0000

    search functionality added and tested successfully

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.restaurants, name='restaurants')
+    path('', views.restaurants, name='restaurants'),
+    path('<restaurant_id>', views.restaurant_details, name='restaurant_details'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Restaurant
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 
 def restaurants(request):
 
     restaurants = Restaurant.objects.all()
+    search_query = None
+    messages.error(request, "No input found, please enter search criteria!")
+    if request.GET:
+        if 'search_text' in request.GET:
+            search_query = request.GET['search_text']
+            if not search_query:
+                messages.error(request, "No input found, please enter search criteria!")
+                return redirect(reverse('restaurants'))
+
+            queries = Q(name__icontains=search_query)
+            restaurants = restaurants.filter(queries)
 
     context = {
         'restaurants': restaurants,
+        'search_term': search_query,
+    }
+    return render(request, 'restaurants/restaurants.html', context)
+
+def restaurant_details(request, restaurant_id):
+
+    restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+
+    context = {
+        'restaurant': restaurant,
     }
 
-    return render(request, 'restaurants/restaurants.html', context)
+    return render(request, 'restaurants/restaurant_details.html', context)
