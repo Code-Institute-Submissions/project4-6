140136efaddc832b9edee0ccc2122c4c52cce016
Moscoder99 <osullivan.martina@gmail.com>
2020-08-22 23:45:34 +0000

    added and successfully tested the shopping bag app and template

--- restaurants/views.py
+++ restaurants/views.py
@@ -11,14 +11,29 @@ def restaurants(request):
     search_query = None
     this_location = None
     this_cuisine = None
+    sort = None
+    direction= None
 
     if request.GET:
+
+        if 'sort' in request.GET:
+            sort_item = request.GET['sort']
+            sort = sort_item
+            if sort_item == 'name':
+                sort_item = 'lower_name'
+                restaurants = restaurants.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sort_item = f'-{sort_item}'
+            restaurants = restaurants.order_by(sort_item)
 
         if 'location' in request.GET:
             this_location = request.GET['location'].split(',')
             restaurants = restaurants.filter(location__name__in=this_location)
             this_location = Location.objects.filter(name__in=this_location)
-        
+
         if 'cuisine' in request.GET:
             this_cuisine = request.GET['cuisine'].split(',')
             restaurants = restaurants.filter(cuisine__name__in=this_cuisine)
@@ -33,11 +48,14 @@ def restaurants(request):
             queries = Q(name__icontains=search_query)
             restaurants = restaurants.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'restaurants': restaurants,
         'search_term': search_query,
         'current_location': this_location,
         'current_cuisine': this_cuisine,
+        'current_sorting': current_sorting,
     }
     return render(request, 'restaurants/restaurants.html', context)
 
