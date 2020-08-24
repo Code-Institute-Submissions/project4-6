202ea581c3a953d03bfdcd3ec4c583008535f27a
Moscoder99 <osullivan.martina@gmail.com>
2020-08-24 12:44:30 +0000

    added updates and removes to shopping bag

--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -21,3 +21,32 @@ def add_to_shoppingbag(request, item_id):
     print(request.session['shoppingbag'])
     print(shoppingbag.items())
     return redirect(redirect_url)
+
+def update_shoppingbag(request, item_id):
+    """ Update the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    shoppingbag = request.session.get('shoppingbag', {})
+
+    if item_id in list(shoppingbag.keys()):
+        shoppingbag[item_id] += quantity
+    else:
+        shoppingbag[item_id] = quantity
+
+    request.session['shoppingbag'] = shoppingbag
+    print(request.session['shoppingbag'])
+    print(shoppingbag.items())
+    return redirect(reverse('shoppingbag'))
+
+def remove_shoppingbag_item(request, item_id):
+    """ Update the shopping bag """
+    try:
+        shoppingbag = request.session.get('shoppingbag', {})
+        bag.pop(item_id)
+
+        request.session['shoppingbag'] = shoppingbag
+        print(request.session['shoppingbag'])
+        print(shoppingbag.items())
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
