369c41bad4967a88e758e6b894469ea78a9fdb37
Moscoder99 <osullivan.martina@gmail.com>
2020-08-28 01:02:08 +0000

    revisions to navbar completed

--- restaurants/models.py
+++ restaurants/models.py
@@ -27,7 +27,7 @@ class Restaurant(models.Model):
     cuisine = models.ForeignKey('Cuisine', null=True, blank=True, on_delete=models.SET_NULL)
     discount = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
